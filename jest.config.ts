22e84e63c692845cb6b4afd50d707d6791bed878
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-01 23:00:30 -0300

    chore: add ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -144,7 +144,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: 'node'
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,9 +173,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: {
-  //   '.+\\.ts$': 'ts-jest'
-  // }
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
