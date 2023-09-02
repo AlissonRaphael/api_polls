7f00db1e2bd9a78f5b3fa6ffcb453bfdb928fad5
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-01 23:03:53 -0300

    feat: implements signup-controller

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,22 @@
+import { type httpResponse, type httpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+
+export class SignUpController {
+  handle (httpRequest: httpRequest): httpResponse {
+    const { name, email } = httpRequest.body
+
+    if (!name) {
+      return badRequest(new MissingParamError('name'))
+    }
+
+    if (!email) {
+      return badRequest(new MissingParamError('email'))
+    }
+
+    return {
+      statusCode: 200,
+      body: {}
+    }
+  }
+}
