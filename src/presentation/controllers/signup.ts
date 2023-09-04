1fba6309b50115220719e1987e988961b8e8a55d
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-04 08:50:53 -0300

    feat: add signup password validations

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,12 @@
 import { type httpResponse, type httpRequest } from '../protocols/http'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { type Controller } from '../protocols/controller'
-import { type EmailValidator } from '../protocols/email-validator'
 import { validationParamRequest } from '../helpers/validation-params-helper'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount
   ) {}
 
   handle (httpRequest: httpRequest): httpResponse {
@@ -17,7 +16,13 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(missingParam))
       }
 
-      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      const emailIsValid = this.emailValidator.isValid(email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
       }
