4195f0fb3f4219e246243842bd02e440c6256853
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-11 09:55:14 -0300

    feat: implements add-account

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,11 @@
 import { type AccountModel } from '../models/account'
 
-interface AddAccountModel {
+export interface AddAccountModel {
   name: string
   email: string
   password: string
 }
 
 export interface AddAccount {
-  add: (account: AddAccountModel) => Promise<AccountModel>
+  add: (account: AddAccountModel) => Promise<AccountModel | null>
 }
