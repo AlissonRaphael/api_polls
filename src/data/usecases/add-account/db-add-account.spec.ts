2f42a7b678b78e1ee546f3d4573e1842a685856e
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-12 07:40:54 -0300

    test: implements account repository

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,21 +1,60 @@
 import { DbAddAccount } from './db-add-account'
+import { type AccountModel, type AddAccountModel, type AddAccountRepository, type Encrypter } from './protocols'
+
+const dbAddAccount = (): any => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => { resolve('hashed_password') })
+    }
+  }
+
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (account: AddAccountModel): Promise<AccountModel> {
+      return await new Promise(resolve => {
+        resolve({
+          id: 0,
+          name: 'Bob',
+          email: 'valid_email@domain.com'
+        })
+      })
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const addAccountRepositoryStub = new AddAccountRepositoryStub()
+  return {
+    create: () => new DbAddAccount(encrypterStub, addAccountRepositoryStub),
+    encrypterStub,
+    addAccountRepositoryStub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => { resolve('hashed_password') })
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const addAccount = new DbAddAccount(encrypterStub)
+    const { create, encrypterStub } = dbAddAccount()
+    const addAccountTest = create()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
-    await addAccount.add({
+    await addAccountTest.add({
       name: 'Bob',
       email: 'valid_email@domain.com',
       password: '12345'
     })
     expect(encryptSpy).toHaveBeenCalledWith('12345')
+  })
+
+  test('Should call AddAccountRepository with correct values', async () => {
+    const { create, addAccountRepositoryStub } = dbAddAccount()
+    const addAccountTest = create()
+    const repositorySpy = jest.spyOn(addAccountRepositoryStub, 'add')
+    await addAccountTest.add({
+      name: 'Bob',
+      email: 'valid_email@domain.com',
+      password: '12345'
+    })
+    expect(repositorySpy).toHaveBeenCalledWith({
+      name: 'Bob',
+      email: 'valid_email@domain.com',
+      password: 'hashed_password'
+    })
   })
 })
