70e949f2dc8c7ce9acbaf8680dde11c2fc7835f7
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-09-26 22:52:25 -0300

    feat: implements route-adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,11 @@
+import { type Request, type Response } from 'express'
+import { type httpResponse, type Controller, type httpRequest } from '../../presentation/protocols'
+
+
+export function AdaptRoute (controller: Controller) {
+  return async (request: Request, response: Response): Promise<void> => {
+    const httpRequest: httpRequest = { body: request.body }
+    const httpResponse: httpResponse = await controller.handle(httpRequest)
+    response.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
