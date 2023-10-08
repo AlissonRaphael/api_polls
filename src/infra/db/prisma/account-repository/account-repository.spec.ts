d37afcc0fa373f04f239e4c9d3c233c228c75b2e
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-10-08 16:15:06 -0300

    refactor: reset table in tests

--- src/infra/db/prisma/account-repository/account-repository.spec.ts
+++ src/infra/db/prisma/account-repository/account-repository.spec.ts
@@ -1,30 +1,28 @@
-import { prismaMock } from '../singleton'
+import prisma from '..'
 import { AccountPostgresRepository } from './account-repository'
 
-const user = {
-  id: 1,
-  name: 'any_name',
-  email: 'any_email@domain.com',
-  password: '12345',
-  createdAt: new Date(Date.now())
-}
-
-prismaMock.account.create.mockResolvedValue(user)
+afterEach(async () => {
+  const resetAccount = prisma.$queryRaw`TRUNCATE TABLE "Account";`
+  const resetAccountIds = prisma.$queryRaw`ALTER SEQUENCE "Account_id_seq" RESTART WITH 1;`
+  await prisma.$transaction([
+    resetAccount,
+    resetAccountIds
+  ])
+})
 
 describe('Account Postgres Repository', () => {
   test('Should return an account on success', async () => {
     const accountPostgresRepository = new AccountPostgresRepository()
-
-    await expect(accountPostgresRepository.add({
+    const account = await accountPostgresRepository.add({
       name: 'any_name',
       email: 'any_email@domain.com',
       password: '12345'
-    })).resolves.toEqual(user)
+    })
 
-    // expect(account).toBeTruthy()
-    // expect(account.id).toBe(1)
-    // expect(account.name).toMatch('any_name')
-    // expect(account.email).toMatch('any_email@domain.com')
-    // expect(account.password).toMatch('12345')
+    expect(account).toBeTruthy()
+    expect(account.id).toBe(1)
+    expect(account.name).toMatch('any_name')
+    expect(account.email).toMatch('any_email@domain.com')
+    expect(account.password).toMatch('12345')
   })
 })
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,5 +1,15 @@
 import request from 'supertest'
 import { app } from '../config/app'
+import prisma from '../../infra/db/prisma'
+
+afterEach(async () => {
+  const resetAccount = prisma.$queryRaw`TRUNCATE TABLE "Account";`
+  const resetAccountIds = prisma.$queryRaw`ALTER SEQUENCE "Account_id_seq" RESTART WITH 1;`
+  await prisma.$transaction([
+    resetAccount,
+    resetAccountIds
+  ])
+})
 
 describe('SignUp Routes', () => {
   test('Should return an account on success', async () => {
