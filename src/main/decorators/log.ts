dacd4e41eeece3a92fc33b3043b36626e6036dac
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-10-10 23:40:54 -0300

    feat: create log error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,13 +1,17 @@
 import { type Controller, type HttpRequest, type HttpResponse } from '../../presentation/protocols'
+import { type LogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export class LogControllerDecorator implements Controller {
-  constructor (public readonly controller: Controller) {}
+  constructor (
+    public readonly controller: Controller,
+    public readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
 
     if (httpResponse.statusCode === 500) {
-      console.log('erro')
+      await this.logErrorRepository.log(httpResponse.body.stack)
     }
 
     return httpResponse
