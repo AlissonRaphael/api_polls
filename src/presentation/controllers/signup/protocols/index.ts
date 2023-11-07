59bb2c0dc96fbbb7481745e942e0779108041d8d
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-11-06 22:10:02 -0300

    feat: implements validation

--- src/presentation/controllers/signup/protocols/index.ts
+++ src/presentation/controllers/signup/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from '../../../protocols'
 export * from '../../../../domain/usecases/add-account'
 export * from './email-validator'
+export * from '../../../helpers/validation'
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,20 +1,19 @@
-import { type HttpResponse, type HttpRequest, type Controller, type EmailValidator, type AddAccount } from './protocols'
-import { MissingParamError, InvalidParamError } from '../../errors'
+import { type HttpResponse, type HttpRequest, type Controller, type EmailValidator, type AddAccount, type Validation } from './protocols'
+import { InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
-import { validationParamRequest } from '../../helpers/validation-params-helper'
 
 export class SignUpController implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly emailValidator: EmailValidator,
     private readonly addAccount: AddAccount
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-      const missingParam = validationParamRequest(httpRequest.body, requiredFields)
-      if (missingParam) {
-        return badRequest(new MissingParamError(missingParam))
+      const paramError = this.validation.validate(httpRequest)
+      if (paramError) {
+        return badRequest(paramError)
       }
 
       const { name, email, password, passwordConfirmation } = httpRequest.body
