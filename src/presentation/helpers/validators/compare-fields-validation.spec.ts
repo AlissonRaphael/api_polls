77452bd8e0a416410b3c92810e9f20bb05078caf
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-11-09 23:57:15 -0300

    test: create compare-fields-validation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeCompareFieldsValidation = (): any => new CompareFieldsValidation('password', 'passwordConfirmation')
+
+describe('CompareField Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const compareFieldsValidation = makeCompareFieldsValidation()
+    const error = compareFieldsValidation.validate({ password: '123' }, { passwordConfirmation: '000' })
+    expect(error).toEqual(new InvalidParamError('password'))
+  })
+
+  test('Should not return validation succeeds', () => {
+    const compareFieldsValidation = makeCompareFieldsValidation()
+    const error = compareFieldsValidation.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
+})
