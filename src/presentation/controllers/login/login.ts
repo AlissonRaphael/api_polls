5c6c9a76f7787dc2c78bb088b1c1a722c8548e49
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-11-14 23:57:10 -0300

    feat: implements validations in login

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,5 @@
 import { type HttpRequest, type HttpResponse, type Controller, type EmailValidator, type Authentication, type Validation } from './protocols'
-import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
-import { validationParamRequest } from '../../helpers/validation-params-helper'
 
 export default class LoginController implements Controller {
   constructor (
@@ -12,17 +10,11 @@ export default class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const missingParam = validationParamRequest(httpRequest.body, ['email', 'password'])
-      if (missingParam) {
-        return badRequest(new MissingParamError(missingParam))
+      const paramError = this.validation.validate(httpRequest.body)
+      if (paramError) {
+        return badRequest(paramError)
       }
-
       const { email, password } = httpRequest.body
-      const emailIsValid = this.emailValidator.isValid(email)
-      if (!emailIsValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
-
       const token = await this.authentication.auth(email, password)
       if (!token) {
         return unauthorized()
