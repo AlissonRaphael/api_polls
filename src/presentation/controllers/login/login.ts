75345e32fc2e34d0278961baedf3a663fccef773
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-12-03 14:19:27 -0300

    refactor: organize http-helpers folder

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { type HttpRequest, type HttpResponse, type Controller, type Authentication, type Validation } from './protocols'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 
 export default class LoginController implements Controller {
   constructor (
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { type HttpResponse, type HttpRequest, type Controller, type AddAccount, type Validation } from './protocols'
-import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 
 export class SignUpController implements Controller {
   constructor (
