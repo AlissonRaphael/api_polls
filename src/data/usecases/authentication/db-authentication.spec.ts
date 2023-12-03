ec5e73a59923ea13ce0e8c07fdde371f396f70bf
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-12-03 16:49:39 -0300

    test: implements hash-comparer

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { DbAuthentication } from './db-authentication'
 import { type AccountModel } from '../add-account/protocols'
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { type HashComparer } from '../../protocols/criptography/hash-comparer'
 
 const fakeAccountModel = {
   id: 0,
@@ -11,8 +12,8 @@ const fakeAccountModel = {
 }
 
 const fakeAuthentication = {
-  name: 'Bob',
-  email: 'valid_email@domain.com'
+  email: 'valid_email@domain.com',
+  password: 'any_password'
 }
 
 const makeDbAuthentication = (): any => {
@@ -22,10 +23,18 @@ const makeDbAuthentication = (): any => {
     }
   }
 
+  class HashComparerStub implements HashComparer {
+    async compare (value: string, hash: string): Promise<boolean> {
+      return await new Promise(resolve => { resolve(true) })
+    }
+  }
+
   const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  const hashComparerStub = new HashComparerStub()
   return {
-    create: () => new DbAuthentication(loadAccountByEmailRepositoryStub),
-    loadAccountByEmailRepositoryStub
+    create: () => new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub),
+    loadAccountByEmailRepositoryStub,
+    hashComparerStub
   }
 }
 
@@ -50,6 +59,30 @@ describe('DbAuthentication UseCase', () => {
     const { create, loadAccountByEmailRepositoryStub } = makeDbAuthentication()
     const dbAuthentication = create()
     jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValue(null)
+    const accessToken = await dbAuthentication.auth(fakeAuthentication)
+    expect(accessToken).toBeNull()
+  })
+
+  test('Should call HashComparer with correct password', async () => {
+    const { create, hashComparerStub } = makeDbAuthentication()
+    const dbAuthentication = create()
+    const compareSpy = jest.spyOn(hashComparerStub, 'compare')
+    await dbAuthentication.auth(fakeAuthentication)
+    expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
+  })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { create, hashComparerStub } = makeDbAuthentication()
+    const dbAuthentication = create()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promiseAccessToken = dbAuthentication.auth(fakeAuthentication)
+    await expect(promiseAccessToken).rejects.toThrow()
+  })
+
+  test('Should return null if HashComparer return false', async () => {
+    const { create, hashComparerStub } = makeDbAuthentication()
+    const dbAuthentication = create()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValue(new Promise(resolve => { resolve(false) }))
     const accessToken = await dbAuthentication.auth(fakeAuthentication)
     expect(accessToken).toBeNull()
   })
