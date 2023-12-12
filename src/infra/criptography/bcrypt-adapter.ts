ba321b6262990ee35c888f7cf943e5b75ce2750b
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-12-11 23:06:39 -0300

    feat: add bcrypt-adapter compare

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,10 +1,15 @@
 import bcrypt from 'bcrypt'
 import { type Hasher } from '../../data/protocols/criptography/hasher'
+import { type HashComparer } from '../../data/protocols/criptography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly salt: number) {}
 
   async hash (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
   }
 }
